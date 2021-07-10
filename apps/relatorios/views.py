a308c73a44d3edf021e599331c3c2faa559da7c7
nathanbahia2 <nathan@datatype.com.br>
2021-07-10 19:30:45 -0300

    [FEAT AND STYLE] - melhorias na interface do sistema e valição de acesso para usuários padrão e administradores

---
+++ apps/relatorios/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render
+from django.contrib.auth.views import login_required
+from django.views.decorators.http import require_POST
+
+from apps.core import models
+from apps.relatorios import forms
+
+
+@login_required
+def index(request):
+    usuario = request.user.perfil.latest('id')
+
+    if usuario.tipo == '1':
+        funcionarios_count = models.Funcionario.objects.filter(filial=usuario.filial).count()
+        elogios_count = models.Elogio.objects.filter(funcionario__filial=usuario.filial).count()
+        ocorrencias_count = models.Ocorrencia.objects.filter(funcionario__filial=usuario.filial).count()
+    else:
+        funcionarios_count = models.Funcionario.objects.count()
+        elogios_count = models.Elogio.objects.count()
+        ocorrencias_count = models.Ocorrencia.objects.count()
+
+    form = forms.RelatorioForm()
+
+    context = {
+        'funcionarios_count': funcionarios_count,
+        'elogios_count': elogios_count,
+        'ocorrencias_count': ocorrencias_count,
+        'form': form
+    }
+    return render(request, 'relatorios/index.html', context)
+
+
+@login_required
+@require_POST
+def gerar(request):
+    usuario = request.user.perfil.latest('id')
+    form = forms.RelatorioForm(data=request.POST)
+
+    data_inicial = None
+    data_final = None
+    tipo_relatorio = None
+    tipo = None
+    query = None
+
+    if form.is_valid():
+        data_inicial = form.cleaned_data.get('data_inicial')
+        data_final = form.cleaned_data.get('data_final')
+        tipo_relatorio = form.cleaned_data.get('tipo')
+
+        if usuario.tipo == '1':
+            filtros = {
+                'data__range': [data_inicial, data_final],
+                'funcionario__filial': usuario.filial
+            }
+        else:
+            filtros = {
+                'data__range': [data_inicial, data_final],
+            }
+
+        if tipo_relatorio == '3':
+            if usuario.tipo == '1':
+                query = models.Funcionario.objects.filter(filial=usuario.filial)
+            else:
+                query = models.Funcionario.objects.all()
+
+            tipo = 'Funcionários'
+
+        else:
+            if data_inicial and data_final:
+                if tipo_relatorio == '1':
+                    query = models.Ocorrencia.objects.filter(**filtros)
+
+                elif tipo_relatorio == '2':
+                    query = models.Elogio.objects.filter(**filtros)
+                    tipo = 'Elogios'
+
+    context = {
+        'data_inicial': data_inicial,
+        'data_final': data_final,
+        'tipo_relatorio': tipo_relatorio,
+        'tipo': tipo,
+        'query': query
+    }
+
+    print(context)
+
+    return render(request, 'relatorios/resultado.html', context)
