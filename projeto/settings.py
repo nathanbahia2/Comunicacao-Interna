f2bf65d08e2a873b06d6c2f4652b8c8f11b39f21
nathanbahia2 <nathan@datatype.com.br>
2021-07-12 17:32:56 -0300

    [HEROKU] - preparação para lançamento no heroku

--- projeto/settings.py
+++ projeto/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b*(ont-dvzmumhlbca$=!8-z8ym9_g8l(%3x$3dwot(1ss0)jl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
